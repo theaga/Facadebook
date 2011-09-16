e773ae662dc8ae5953f87cd78b9dfd7b41d54974
Daniel Saidi <daniel.saidi@gmail.com>
2011-09-16 13:35:50 +0200

    First class checkin

---
+++ FacebookAbstractions/Abstractions/IFacebookApplicationFacade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Facebook;
+
+namespace FacebookAbstractions.Abstractions
+{
+    /// <summary>
+    /// This interface can be implemented by all classes that should be
+    /// be able to be used as a facade for the FacebookApplication class.
+    /// </summary>
+    public interface IFacebookApplicationFacade
+    {
+        /// <summary>
+        /// Get the current Facebook application.
+        /// </summary>
+        IFacebookApplication Current { get; }
+
+        /// <summary>
+        /// Gets InnerCurrent.
+        /// </summary>
+        IFacebookApplication InnerCurrent { get; }
+
+
+        /// <summary>
+        /// Set the inner application.
+        /// </summary>
+        /// <param name="getFacebookApplication">The get Facebook application.</param>
+        void InnerSetApplication(Func<IFacebookApplication> getFacebookApplication);
+
+        /// <summary>
+        /// Set the inner application.
+        /// </summary>
+        /// <param name="facebookApplication">The Facebook application.</param>
+        void InnerSetApplication(IFacebookApplication facebookApplication);
+    }
+}
---
+++ FacebookAbstractions/FacebookApplicationFacade.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Facebook;
+using FacebookAbstractions.Abstractions;
+
+namespace FacebookAbstractions
+{
+    /// <summary>
+    /// This class can be used as a facade for the FacebookApplication class.
+    /// </summary>
+    public class FacebookApplicationFacade : FacadeBase<FacebookApplication>, IFacebookApplicationFacade
+    {
+        /// <summary>
+        /// Create an instance of the facade class.
+        /// </summary>
+        /// <param name="baseInstance">The base instance to facade.</param>
+        public FacebookApplicationFacade(FacebookApplication baseInstance) : base(baseInstance)
+        {
+        }
+
+
+        /// <summary>
+        /// Get the current Facebook application.
+        /// </summary>
+        public IFacebookApplication Current
+        {
+            get { return FacebookApplication.Current; }
+        }
+
+        /// <summary>
+        /// Gets InnerCurrent.
+        /// </summary>
+        public IFacebookApplication InnerCurrent
+        {
+            get { return BaseInstance.InnerCurrent; }
+        }
+
+
+        /// <summary>
+        /// Set the inner application.
+        /// </summary>
+        /// <param name="getFacebookApplication">The get Facebook application.</param>
+        public void InnerSetApplication(Func<IFacebookApplication> getFacebookApplication)
+        {
+            BaseInstance.InnerSetApplication(getFacebookApplication);
+        }
+
+        /// <summary>
+        /// Set the inner application.
+        /// </summary>
+        /// <param name="facebookApplication">The Facebook application.</param>
+        public void InnerSetApplication(IFacebookApplication facebookApplication)
+        {
+            BaseInstance.InnerSetApplication(facebookApplication);
+        }
+    }
+}
