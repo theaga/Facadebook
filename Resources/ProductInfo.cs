4bac34fe2507bba950f1f6c4439f9c66af14c92e
Daniel Saidi <daniel.saidi@gmail.com>
2011-09-29 15:16:18 +0200

    Added new 5.2.1.0 references

--- Resources/ProductInfo.cs
+++ Resources/ProductInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.2.1.0")]
-[assembly: AssemblyFileVersion("5.2.1.0")]
+[assembly: AssemblyVersion("5.2.1.3")]
+[assembly: AssemblyFileVersion("5.2.1.3")]
